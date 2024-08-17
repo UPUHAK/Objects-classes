96436034bc8be35093d949c3a948c4fb96f00d1b
UPUHAK <89092936911@mail.ru>
2024-08-17 04:02:02 +0300

    homework-2 is done

--- src/App.java
+++ src/App.java
@@ -2,27 +2,23 @@ public class App {
     public static void main(String[] args) {
 
         Author gromyko = new Author("Ольга ", "Громыко");
-        System.out.println(gromyko.getName() + gromyko.getSurname());
+        System.out.println(gromyko);
 
         Author shevchenko = new Author("Ирина ", "Шевченко");
-        System.out.println(shevchenko.getName() + shevchenko.getSurname());
+        System.out.println(shevchenko);
 
 
         Book cybercanicles = new Book("Киберканикулы", gromyko, 2022);
-        bookInformPrint(cybercanicles);
+        System.out.println(cybercanicles);
 
         Book islandOfNotGuilty = new Book("Остров невиновных", shevchenko, 2020);
-        bookInformPrint(islandOfNotGuilty);
+        System.out.println(islandOfNotGuilty);
 
         cybercanicles.setYear(2023);
 
-        bookInformPrint(cybercanicles);
+        System.out.println(cybercanicles);
 
     }
 
-    public static void bookInformPrint(Book book) {
-        System.out.println("Книга '" + book.getTitle() + "'. Автор " + book.getAuthor().getName() + book.getAuthor().getSurname()
-                + ". Год выпуска " + book.getYear() + ".");
 
-    }
 }
